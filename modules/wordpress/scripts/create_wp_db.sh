23e693db73e776c9453ffdd20c35228987cd5a3a
Frederic Descamps <lefred.descamps@gmail.com>
2020-09-22 10:18:24 +0200

    fix wrong character issue
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- modules/wordpress/scripts/create_wp_db.sh
+++ modules/wordpress/scripts/create_wp_db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
 
-mysqlsh ${admin_username}:${admin_password}@${mds_ip} --sql -e "CREATE DATABASE ${wp_schema};"
-mysqlsh ${admin_username}:${admin_password}@${mds_ip} --sql -e "CREATE USER ${wp_name} identified by '${wp_password}';"
-mysqlsh ${admin_username}:${admin_password}@${mds_ip} --sql -e "GRANT ALL PRIVILEGES ON ${wp_schema}.* TO ${wp_name};"
+mysqlsh ${admin_username}:'${admin_password}'@${mds_ip} --sql -e "CREATE DATABASE ${wp_schema};"
+mysqlsh ${admin_username}:'${admin_password}'@${mds_ip} --sql -e "CREATE USER ${wp_name} identified by '${wp_password}';"
+mysqlsh ${admin_username}:'${admin_password}'@${mds_ip} --sql -e "GRANT ALL PRIVILEGES ON ${wp_schema}.* TO ${wp_name};"
 
 echo "WordPress Database and User created !"
