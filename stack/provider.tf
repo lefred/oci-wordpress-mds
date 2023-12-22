b7a7590737b98e431c98bf523cdba5ec9cb7868c
Frederic Descamps <lefred.descamps@gmail.com>
2023-12-22 16:09:44 +0100

    add provider
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- stack/provider.tf
+++ stack/provider.tf
@@ -5,3 +5,12 @@ provider "oci" {
   fingerprint = var.fingerprint
   private_key_path = var.private_key_path
 }
+
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+    }
+  }
+}
+
